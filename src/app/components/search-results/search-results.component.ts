24195f18143013ab0743f0df6e8a1c8fd15d304d
unknown <dave_ik2010@yahoo.com>
2020-09-02 10:19:28 +0100

    Use searchterm as parameter for service function to get data and store in all movies to be passes dowm to movie-item

---
+++ src/app/components/search-results/search-results.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { MovieServiceService } from '../../services/movie-service.service'
+import { movieData } from '../../models/movieData'
+@Component({
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.css']
+})
+export class SearchResultsComponent implements OnChanges, OnInit {
+@Input() searchterm:string;
+allMovies:movieData;
+test = 3;
+
+  constructor(private service: MovieServiceService) { }
+
+  ngOnInit(): void {
+
+    }
+  
+  ngOnChanges(changes: SimpleChanges){
+      if(this.searchterm){
+         this.service.getMovies(this.searchterm)
+        .subscribe(data=>{
+          this.allMovies = data.Search;
+          
+        })
+      }
+     
+      
+
+    }
+  
+  
+
+
+}
+  
